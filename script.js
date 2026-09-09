e3ba282aa46c40b7d892e49e4607d955c75cebe5
TranDucKhiem14082005 <khiemtran015@gmail.com>
2026-09-09 22:50:09 +0700

    khiem_new

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 console.log("heello mọi người xin chào mọi người đến với thế giới học tập của mình");
 alert("do you want to learn JavaScript?");
+console.log("yes, I want to learn JavaScript");
