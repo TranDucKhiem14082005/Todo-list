046ed6e2009bb2e721073b067c70754e8b4bd671
TranDucKhiem14082005 <khiemtran015@gmail.com>
2026-09-09 22:38:09 +0700

    new code

---
+++ script.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "demo for learn git and github"
+git branch -M main
+git remote add origin git@github.com:TranDucKhiem14082005/Todo-list.git
+git push -u origin main
